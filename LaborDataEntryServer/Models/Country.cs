45560cd14de50d2c1840238a2b90100b6e445afe
Sanjeev Chaudhary <sunziv01@hotmail.com>
2022-11-24 08:07:41 +0545

    Making Database good

--- LaborDataEntryServer/Models/Country.cs
+++ LaborDataEntryServer/Models/Country.cs
@@ -13,7 +13,8 @@ namespace LaborDataEntryServer.Models
         public string  Name{ get; set; }
         public string Code { get; set; }
         public bool IsActive{ get; set; }
-        public ICollection<District> District { get; set; } //Country one-to-many District
+
+        public ICollection<District> District; //Country one-to-many District
         
         public ICollection<Labor> Labor;
     }
