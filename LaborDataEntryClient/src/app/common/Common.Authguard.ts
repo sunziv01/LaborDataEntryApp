815b45ddc7f66049eb575f3eebbed22e4092fb04
Sanjeev Chaudhary <sunziv01@hotmail.com>
2022-11-29 08:56:22 +0545

    .

--- LaborDataEntryClient/src/app/common/Common.Authguard.ts
+++ LaborDataEntryClient/src/app/common/Common.Authguard.ts
@@ -15,7 +15,7 @@ export class MyAuthGuard implements CanActivate {
         return true;
     }
 
-    // navigate to login page
+    // Navigate to login page
     this._router.navigate(['/Login']);
     // you can save redirect url so after authing we can move them back to the page they requested
     return false;
